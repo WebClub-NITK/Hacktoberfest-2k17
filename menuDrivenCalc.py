29326617d755fccde05e1cd0e3ff570349faea7e
geekgeekvm <mbarbie.vaishu@gmail.com>
2017-10-05 13:09:42 +0530

    Menu driven calculator in python using eval

---
+++ menuDrivenCalc.py
@@ -0,0 +1,11 @@
+def main():
+    a=(input("Enter first operand"))
+    b=(input("Enter second operand"))
+    op=input("Enter operator")
+    res=eval(a+op+b)
+    print("Result is: ",res)
+    
+    
+
+if __name__=='__main__':
+    main()
