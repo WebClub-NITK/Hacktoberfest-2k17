37d9436244347cc1b028a3fa561b32fcf0d61ddc
ramramki26 <32484925+ramramki26@users.noreply.github.com>
2017-10-07 22:26:15 +0530

    Create ramramki26.c

---
+++ BinarySearch/ramramki26.c
@@ -0,0 +1,49 @@
+#include<iostream>
+int main()
+{
+ int a[20],beg,mid,end,n,i,ele,c=-1;
+ cout<<"Enter number of elements"<<endl;
+ 
+ 
+ for(i=0;i<n;i++)
+ {
+  cin>>a[i];
+ }
+ cout<<"Enter search element"<<endl;
+ cin>>ele;
+ for(i=n-1;i>0;i--)
+ {
+  for(int j=0;j<i;j++)
+  {
+   int t;
+   a[j]=t;
+   a[j]=a[j+1};
+   a[j+1]=t;
+  }
+ }
+ beg=0;
+ end=n-1;
+ while(beg<=end)
+ {
+  mid=(beg+end)/2;
+  if(ele>a[mid])
+  {
+   beg=mid+1;
+  }
+  else if(ele<a[mid])
+  {
+   end=mid-1;
+   }
+  else 
+  {
+   c=mid;
+   break;
+  }
+ }
+ if(c==-1)
+ cout<<ele<<" not found";
+ else
+ cout<<ele<" found at position"<<(c+1);
+ return 0;
+} 
+
