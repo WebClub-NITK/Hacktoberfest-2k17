70b2429082503427ea2cdc27acd2c6d1648f5f3a
ApoorvAgarwal21 <32730652+ApoorvAgarwal21@users.noreply.github.com>
2017-10-19 07:49:09 +0530

    Update and rename program to add two numbers in python to program to add two numbers in python.py

--- program to add two numbers in python.py
+++ program to add two numbers in python.py
@@ -1,4 +1,4 @@
-code :-
+
 a = int(raw_input())                   # allows to take first input from user
 b = int(raw_input())                   # allows to take second input from user
 print a+b                              # prints the sum a+b
