67aab4f0278e8df7bc5cf9219d48ed755f780f11
Apeksha1997 <32430370+Apeksha1997@users.noreply.github.com>
2017-10-28 23:57:34 +0530

    Create insertionsort.c

---
+++ insertionsort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX 7
+ 
+void insertion_sort(int *);
+ 
+void main()
+{
+    int a[MAX], i;
+ 
+    printf("enter elements to be sorted:");
+    for (i = 0;i < MAX;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    insertion_sort(a);
+    printf("sorted elements:\n");
+    for (i = 0;i < MAX; i++)
+    {
+        printf(" %d", a[i]);
+    }
+}
+ 
+void insertion_sort(int * x)
+{
+    int temp, i, j;
+ 
+    for (i = 1;i < MAX;i++)
+    {
+        temp = x[i];
+        j = i - 1;
+        while (temp < x[j] && j >= 0)
+        {
+            x[j + 1] = x[j];
+            j = j - 1;
+        }
+        x[j + 1] = temp;
+    }
+}
