0374662748e8c4ade58e25e329ebd0f83ed59ba8
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2017-10-29 13:50:36 +0400

    Changed the bubble sort to more optimized version

--- Bubble_Sort/sisgandarli.c
+++ Bubble_Sort/sisgandarli.c
@@ -1,14 +1,12 @@
-// C program for implementation of Bubble sort
 #include <stdio.h>
  
-void swap(int *xp, int *yp)
+void swap(int *a, int *b)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
  
-// A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
    int i, swapped;
@@ -28,22 +26,21 @@ void bubbleSort(int arr[], int n)
    }
 }
  
-/* Function to print an array */
 void printArray(int arr[], int size)
 {
     int i;
-    for (i=0; i < size; i++)
+
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
  
-// Driver program to test above functions
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {32, 1, 89, 54, 9, 7, 67};
+    int n = sizeof(arr) / sizeof(arr[0]);
     bubbleSort(arr, n);
-    printf("Sorted array: \n");
+    printf("Sorted array:\n");
     printArray(arr, n);
     return 0;
 }
