c990766b5aba9aa06be96eff191460feb305839b
krahulreddy <k_rahul_reddy@outlook.com>
2017-10-30 19:38:30 +0530

    Hello World in C added

--- Binary Search/krahulreddy.cpp
+++ Binary Search/krahulreddy.cpp
@@ -2,29 +2,31 @@
 using namespace std;
 
 int main(){
-int a[100], n, ele, low=0, mid, high, pos=-1;
-cout<<"Enter the number of elements : ";
-cin>>n;
-cout<<"Enter the elements : ";
-for(int i=0;i<n;i++)
-cin>>a[i];
-cout<<"Enter Search element : ";
-cin>>ele;
-high=n-1;
-while(low<=high)
+  int a[100], n, ele, first=0, mid, last, pos=-1;
+  cout<<"Enter the number of elements : ";
+  cin>>n;
+  for(int i=0;i<n;i++){
+	cout<<"Enter the elements : ";
+	cin>>a[i];
+  }
+  cout<<"Enter Search element : ";
+  cin>>ele;
+  last=n-1;
+  while(first<=last)
   {
-    mid=(low+high)/2;
+    mid=(first+last)/2;
     if(ele<a[mid])
-        high=mid-1;
+        last=mid-1;
     else if(ele>a[mid])
-        low=mid+1;
+        first=mid+1;
     else{
         pos=mid+1;
         break;
     }
   }
-if(pos>=0)
-cout<<ele<<" found at "<<pos<<"th position";
-else
-cout<<"Given element not found in the list"<<endl;
+  if(pos>=0)
+  cout<<ele<<" found at "<<pos<<"th position";
+  else
+  cout<<"Given element not found in the list"<<endl;
+  return 0;
 }
